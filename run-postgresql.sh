7b88bc2b23e62624e9f4f02b0ccc80a5f638014d
hank <hankzhangz@163.com>
2017-05-19 17:50:29 +0800

    sdfdf

--- run-postgresql.sh
+++ run-postgresql.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-. ~/.bash_profile
-pg_ctl start -l /var/lib/pgsql/9.4/pgstartup.log
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+grep -v ^postgres /etc/passwd > "$HOME/9.4/user/passwd"
+echo   "postgres:x:${USER_ID}:${GROUP_ID}:PostgreSQL Server:${HOME}:/bin/bash" >> "$HOME/9.4/user/passwd"
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWORD=${HOME}/9.4/user/passwd
+export NSS_WRAPPER_GROUP=/etc/group
 
-sleep 3
+echo -n "Initialize postgresql"
+if [ ! -d /var/lib/pgsql/9.4/data/userdata/ ];then
+	echo -n "userdata dir not exists"
+	mkdir /var/lib/pgsql/9.4/data/userdata/
+	chmod 700 /var/lib/pgsql/9.4/data/userdata/
+fi
 
-psql --command "ALTER USER postgres WITH PASSWORD 'postgres';"
-psql --command "CREATE DATABASE prpc WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' CONNECTION LIMIT = -1;"
-psql --command "ALTER ROLE postgres IN DATABASE prpc SET search_path = pegarules, pegadata;"
-psql --command "CREATE ROLE pegadata LOGIN ENCRYPTED PASSWORD 'md53d879fa3e037a1a389ff6d61da15381e' SUPERUSER INHERIT CREATEDB NOCREATEROLE NOREPLICATION;"
-psql --command "CREATE ROLE pegarules LOGIN ENCRYPTED PASSWORD 'md543f2fe59c9fee2e2fa57dba080c59b04' SUPERUSER INHERIT CREATEDB NOCREATEROLE NOREPLICATION;"
-psql -d prpc --command "CREATE SCHEMA pegadata AUTHORIZATION pegadata;"
-psql -d prpc --command "GRANT ALL ON SCHEMA pegadata TO pegadata;"
-psql -d prpc --command "GRANT ALL ON SCHEMA pegadata TO public;"
-psql -d prpc --command "CREATE SCHEMA pegarules AUTHORIZATION pegarules;"
-psql -d prpc --command "GRANT ALL ON SCHEMA pegarules TO pegarules;"
-psql -d prpc --command "GRANT ALL ON SCHEMA pegarules TO public;"
-psql -d prpc < /var/lib/pgsql/9.4/install.sql
-pg_ctl stop
+if [ -f /var/lib/pgsql/9.4/data/userdata/finnish ];then
+	echo "data has exists"
+else
+	rm -rf /var/lib/pgsql/9.4/data/userdata/*
+	cd /var/lib/pgsql/9.4/data/userdata/
+	wget -r -nH --cut-dirs=1 . http://pegadata.gl-internal.huawei.com/pega_data/
+	touch finnish
+fi
+echo -n "start postgres daemon"
 exec postgres "$@"
